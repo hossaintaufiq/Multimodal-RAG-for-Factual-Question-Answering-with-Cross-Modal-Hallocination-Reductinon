26e33d365f33b879c2818a74d89577b6fef7f7f1
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2026-03-15 22:25:56 +0600

    orientation done

---
+++ Rag System Testing/PlaneRag.py
@@ -0,0 +1,3 @@
+print('This is Rag system testing part 1')
+print('Understanding and analizing the prototypes of old system and try to make them ')
+print('Collect and observe them and try to impliment into the new system')
